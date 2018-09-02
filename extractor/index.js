b657961b7c1189bf12ceaaadcea1293f38e1babb
Hugo Grochau <hugo@grochau.com>
2018-09-02 13:28:40 -0300

    Add login and search page extraction

--- extractor/index.js
+++ extractor/index.js
@@ -1,1 +1,9 @@
-console.info('Hello World')
+import fs from 'fs'
+import { logger } from './logger'
+if (!fs.existsSync('./config.json')) {
+  logger.error('Please copy config.example.json to config.json with the correct data')
+  process.exit(1)
+}
+
+import { extract } from './extract' // eslint-disable-line import/first
+extract()
