b3e828102fc3b1e792ba1d68e97786bf094073d5
Hugo Grochau <hugo@grochau.com>
2018-09-04 17:54:12 -0300

    Remove debug content.html

--- extractor/topCompaniesOfSchools/getSchoolIdByName.js
+++ extractor/topCompaniesOfSchools/getSchoolIdByName.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 const buildSchoolUrl = (schoolName) => `https://linkedin.com/school/${schoolName}/`
 const schoolIdRegex = /headquarter","school":"urn:li:fs_normalized_school:(\d*)/
 
@@ -7,7 +6,6 @@ export const getSchoolIdByName = async (page, schoolName) => {
   await page.goto(schoolUrl)
 
   const content = await page.content()
-  fs.writeFileSync('content.html', content)
   const [, schoolId] = content.match(schoolIdRegex)
 
   return parseInt(schoolId, 10)
