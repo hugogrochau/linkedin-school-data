a7f66044ac04bd0a86abae642466e1939b8c8435
Hugo Grochau <hugo@grochau.com>
2018-11-04 15:42:00 -0200

    Alphabetically sort correlation table

--- visualizer/src/data/correlationTable.ts
+++ visualizer/src/data/correlationTable.ts
@@ -3,24 +3,62 @@ import correlationTableJson from './correlationTable.json'
 
 import { getSchoolDataByName } from './schoolData'
 
-export const getCorrelationTable = () => correlationTableJson
-
 type CorrelationItem = [number, number, number]
-export const getCorrelationTableWeighted = () => {
-  const { correlation } = correlationTableJson
+interface CorrelationTable {
+  schools: string[],
+  companies: string[],
+  correlation: CorrelationItem[]
+}
 
-  const weightedCorrelation = correlation.map(weightCorrelation)
+export const getCorrelationTable = (): CorrelationTable => {
+  const { schools, companies, correlation } = correlationTableJson
+  const sortedSchools = R.sort(R.ascend(R.identity), schools) as string[]
+  const sortedCompanies = R.sort(R.descend(R.identity), companies) as string[]
+
+  const schoolsChangedIndexes = getChangedIndexes(schools, sortedSchools)
+  const companiesChangedIndexes = getChangedIndexes(companies, sortedCompanies)
+
+  const mapCorrelation = ([school, company, correlation]: CorrelationItem) => [
+    schoolsChangedIndexes[school],
+    companiesChangedIndexes[company],
+    correlation
+  ]
+
+  const sortedCorrelation = correlation.map(mapCorrelation)
 
   return {
-    ...correlationTableJson,
+    schools: sortedSchools,
+    companies: sortedCompanies,
+    correlation: sortedCorrelation
+  }
+
+}
+
+const getChangedIndexes = (oldArray: any[], newArray: any[]): {[oldIndex: number]: number} => {
+  const newIndexFormatFn = (value: any, index: number) => ({
+    [index]: R.findIndex(R.equals(value))(newArray)
+  })
+
+  const newIndexes = oldArray.map(newIndexFormatFn)
+
+  return R.mergeAll(newIndexes)
+}
+
+export const getCorrelationTableWeighted = () => {
+  const correlationTable = getCorrelationTable()
+  const { correlation, schools } = correlationTable
+
+  const weightedCorrelation = correlation.map(weightCorrelation(schools))
+
+  return {
+    ...correlationTable,
     correlation: weightedCorrelation
   }
 
 }
 
-const weightCorrelation = ([school, company, occurences]: CorrelationItem) => {
+const weightCorrelation = (schools: string[]) => ([school, company, occurences]: CorrelationItem) => {
   const schoolDataByName = getSchoolDataByName()
-  const { schools } = correlationTableJson
   const schoolName = schools[school]
   const { alumni } = schoolDataByName[schoolName]
   const alumniNumber = parseInt(alumni, 10)
